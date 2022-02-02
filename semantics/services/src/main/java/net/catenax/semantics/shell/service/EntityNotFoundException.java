e451ecf9b72b9265259ff640f5a2afa4ee5c1ddc
Fethullah Misir <fethullah.misir@de.bosch.com>
2022-02-02 12:11:06 +0100

    Rename package

--- semantics/services/src/main/java/net/catenax/semantics/shell/service/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package net.catenax.semantics.shell.service;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message){
-        super(message);
-    }
-}
